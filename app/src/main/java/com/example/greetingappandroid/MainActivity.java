55bbf46793196fc14ac03733a966c8b474501ad4
DELL <omnianabil1920@gmail.com>
2024-07-04 21:03:10 +0300

    finish simple app

--- app/src/main/java/com/example/greetingappandroid/MainActivity.java
+++ app/src/main/java/com/example/greetingappandroid/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.greetingappandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    Button myBtn;
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText2);
+        myBtn = findViewById(R.id.button2);
+        title = findViewById(R.id.textView);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = editText.getText().toString();
+                Toast.makeText(MainActivity.this , "Welcome " + inputName + " to our app" , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
